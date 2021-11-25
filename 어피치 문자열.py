2704d677e7966fbc721372a9cdaef4b96b01655e
parkbyong123 <81605930+parkbyong123@users.noreply.github.com>
2021-11-25 17:36:59 +0900

    Update 어피치 문자열.py

--- 어피치 문자열.py
+++ 어피치 문자열.py
@@ -1,16 +1,12 @@
-s = "abcabcabcabcdededededede"
 def solution(s):
-    lens = int(len(s)/2)
-    comp = []
-    # 한글자 예외처리
     if len(s) == 1:
         return 1
-    
-    for i in range(1, lens + 2): #나누는 수
+    lens = int(len(s)/2)
+    comp = []
+    for i in range(1, lens + 1): #나누는 수
         string_s = ""
         count_n = 1
         for j in range(0, len(s), i): # 문자열 비교
-            #print("j" + str(j))
             if s[j:j+i] == s[j+i:j+i+i]:
                 count_n += 1
             else:
@@ -23,5 +19,3 @@ def solution(s):
     answer = min(comp)
     
     return answer
-    
-solution(s)
